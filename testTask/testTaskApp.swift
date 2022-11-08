517f078f4a9a9686c8040d4ac39b2e97c43a1e76
Алексей Иванов <ivanovad97@gmail.com>
2022-11-08 15:09:57 +0500

    Initial Commit

---
+++ testTask/testTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  testTaskApp.swift
+//  testTask
+//
+//  Created by Алексей on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct testTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
